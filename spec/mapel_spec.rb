90bba96b590b39846539dec452a66b5a05d3c293
Andre Pankratz <andre@webwarelab.com>
2011-03-17 15:23:45 +0100

    Refactored specs

--- spec/mapel_spec.rb
+++ spec/mapel_spec.rb
@@ -1,15 +1,8 @@
 require File.dirname(__FILE__) + '/spec_helper.rb'
 
 describe Mapel do
-  before do
-    @input    = File.dirname(__FILE__) + '/fixtures'
-    @output   = File.dirname(__FILE__) + '/output'
-    @logo     = @input + '/ImageMagick.jpg'
-  end
-
-  after do
-    Dir.glob(@output + '/*') { |f| File.delete(f) }
-  end
+  before {setup}
+  after {teardown}
 
   it "should respond to #info" do
     Mapel.respond_to?(:info).should == true
@@ -20,48 +13,49 @@ describe Mapel do
   end
 
   it "should support compact rendering syntax" do
-    Mapel(@logo).should.be.kind_of(Mapel::Engine)
+    Mapel(logo).should.be.kind_of(Mapel::Engine)
   end
 
   describe "#info" do
     it "should return basic image metadata" do
-      Mapel.info(@logo)[:path].should == @logo
-      Mapel.info(@logo)[:format].should == 'JPEG'
-      Mapel.info(@logo)[:dimensions].should == [572, 591]
-      Mapel.info(@logo)[:depth].should == '8-bit'
-      Mapel.info(@logo)[:size].should == '95.1kb'
+      info = Mapel.info(logo)
+      info[:path].should == logo
+      info[:format].should == 'JPEG'
+      info[:dimensions].should == [572, 591]
+      info[:depth].should == '8-bit'
+      info[:size].should == '95.1kb'
     end
   end
 
   describe "#render" do
     it "should be able to scale an image" do
-      cmd = Mapel(@logo).scale('50%').to(@output + '/scaled.jpg').run
+      cmd = Mapel(logo).scale('50%').to(out_folder + '/scaled.jpg').run
       cmd.status.should == true
-      Mapel.info(@output + '/scaled.jpg')[:dimensions].should == [286, 296]
+      Mapel.info(out_folder + '/scaled.jpg')[:dimensions].should == [286, 296]
     end
 
     it "should be able to crop an image" do
-      cmd = Mapel(@logo).crop('50x50+0+0').to(@output + '/cropped.jpg').run
+      cmd = Mapel(logo).crop('50x50+0+0').to(out_folder + '/cropped.jpg').run
       cmd.status.should == true
-      Mapel.info(@output + '/cropped.jpg')[:dimensions].should == [50, 50]
+      Mapel.info(out_folder + '/cropped.jpg')[:dimensions].should == [50, 50]
     end
 
     it "should be able to resize an image" do
-      cmd = Mapel(@logo).resize('100x').to(@output + '/resized.jpg').run
+      cmd = Mapel(logo).resize('100x').to(out_folder + '/resized.jpg').run
       cmd.status.should == true
-      Mapel.info(@output + '/resized.jpg')[:dimensions].should == [100, 103]
+      Mapel.info(out_folder + '/resized.jpg')[:dimensions].should == [100, 103]
     end
 
     it "should be able to crop-resize an image" do
-      cmd = Mapel(@logo).gravity(:west).resize!('50x100').to(@output + '/crop_resized.jpg').run
+      cmd = Mapel(logo).gravity(:west).resize!('50x100').to(out_folder + '/crop_resized.jpg').run
       cmd.status.should == true
-      Mapel.info(@output + '/crop_resized.jpg')[:dimensions].should == [50, 100]
+      Mapel.info(out_folder + '/crop_resized.jpg')[:dimensions].should == [50, 100]
     end
 
     it "should allow arbitrary addition of commands to the queue" do
-      cmd = Mapel(@logo).gravity(:west)
+      cmd = Mapel(logo).gravity(:west)
       cmd.resize(50, 50)
-      cmd.to_preview.should == "convert #{@logo} -gravity west -resize \"50x50\""
+      cmd.to_preview.should == %(convert #{logo} -gravity west -resize "50x50")
     end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,4 +3,29 @@ require 'bacon'
 
 require File.dirname(__FILE__) + '/../lib/mapel'
 
+def in_folder
+  @in_folder ||= File.dirname(__FILE__) + '/fixtures'
+end
+
+def out_folder
+  @out_folder ||= File.dirname(__FILE__) + '/output'
+end
+
+def logo
+  @logo ||= in_folder + '/ImageMagick.jpg'
+end
+
+def setup
+  unless File.exists?(out_folder)
+    Dir.mkdir(out_folder)
+  end
+end
+
+def teardown
+  if File.exists?(out_folder)
+    Dir.glob(out_folder + '/*') { |f| File.delete(f) }
+    Dir.rmdir(out_folder)
+  end
+end
+
 Bacon.summary_on_exit
